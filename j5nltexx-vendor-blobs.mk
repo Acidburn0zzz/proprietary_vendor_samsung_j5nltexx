54f9d918ecc2c60c99cb2eeee04cf834f6160ac3
Nick Verse <nikoskalomiris@gmail.com>
2016-05-13 07:23:11 +0300

    Update j5nltexx-vendor-blobs.mk

--- j5nltexx-vendor-blobs.mk
+++ j5nltexx-vendor-blobs.mk
@@ -22,4 +22,3 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j5nltexx/proprietary/lib/libsec-ril.so:system/lib/libsec-ril.so \
     vendor/samsung/j5nltexx/proprietary/lib/libsecril-client.so:system/lib/libsecril-client.so \
     vendor/samsung/j5nltexx/proprietary/lib/libaudio-ril.so:system/lib/libaudio-ril.so
-
