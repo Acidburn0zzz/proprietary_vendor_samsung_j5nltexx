f236d0ff9c752a271dda603d5b5881b06a00e912
nickverse <nikoskalomiris@gmail.com>
2016-07-02 23:35:34 +0300

    NFC proprietary test

--- j5nltexx-vendor.mk
+++ j5nltexx-vendor.mk
@@ -17,5 +17,10 @@
 # Pick up overlay for features that depend on non-open-source files
 DEVICE_PACKAGE_OVERLAYS += vendor/samsung/j5nltexx/overlay
 
+PRODUCT_PACKAGES += \
+    NfcNci
+
+PRODUCT_PACKAGES += \
+    Tag
 
 $(call inherit-product, vendor/samsung/j5nltexx/j5nltexx-vendor-blobs.mk)
