ef3574ea96c9e64072d7cefe540cefc9f4d9ef7a
nickverse <nikoskalomiris@gmail.com>
2016-12-16 15:52:50 +0200

    Add rmnetcli

--- j5nltexx-vendor-blobs.mk
+++ j5nltexx-vendor-blobs.mk
@@ -19,6 +19,7 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/j5nltexx/proprietary/lib/hw/nfc_nci.MSM8916.so:system/lib/hw/nfc_nci.MSM8916.so \
     vendor/samsung/j5nltexx/proprietary/lib/libnfc-nci.so:system/lib/libnfc-nci.so \
     vendor/samsung/j5nltexx/proprietary/bin/rild:system/bin/rild \
+    vendor/samsung/j5nltexx/proprietary/bin/rmnetcli:system/bin/rmnetcli \
     vendor/samsung/j5nltexx/proprietary/vendor/lib/libril-qcril-hook-oem.so:system/vendor/lib/libril-qcril-hook-oem.so \
     vendor/samsung/j5nltexx/proprietary/lib/librmnetctl.so:system/lib/librmnetctl.so \
     vendor/samsung/j5nltexx/proprietary/lib/libril.so:system/lib/libril.so \
